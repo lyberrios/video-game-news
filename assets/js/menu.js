86e8d0baab4e013ae78fc8a26f884d3b2660e1e5
Ermarly Berrios <lyberryo@gmail.com>
2025-03-24 23:02:50 -0300

    Add interactive menu and submenu JS and finish project

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,16 +3,46 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let mobileBtn = document.querySelector(".navbar__mobile--btn");
     let mobileMenu = document.querySelector(".navbar__mobile");
 
-    mobileBtn.addEventListener("click", () => {  
+    mobileBtn.addEventListener("click", () => {
         let menuOpen = document.querySelector(".menuOpen");
-        
+
         if (!menuOpen) {
             mobileMenu.style.display = "block";
             mobileMenu.classList.add("menuOpen");
-        }else{
+        } else {
             mobileMenu.style.display = "none";
             mobileMenu.classList.remove("menuOpen");
         }
     });
+    const submenu = (button, submenu, className) => {
+        button.addEventListener("click", () => {
+            let drop = document.querySelector("." + className);
+            if (!drop) {
+                submenu.style.display = "block";
+                submenu.classList.add(className);
+            } else {
+                submenu.style.display = "none";
+                submenu.classList.remove(className);
+            }
+        })
+    }
+    //Primer submenu
+    let dropdown1Btn = document.querySelector("#icon-1");
+    let dropdown1Submenu = document.querySelector("#submenu1");
+    //Segundo submenu
+    let dropdown2Btn = document.querySelector("#icon-2");
+    let dropdown2Submenu = document.querySelector("#submenu2");
+    //Ejecutar funcionalidad
+    submenu(dropdown1Btn, dropdown1Submenu, "drop1");
+    submenu(dropdown2Btn, dropdown2Submenu, "drop2");
 
+    // Resized - redimensionado
+    window.addEventListener("resize", () => {
+        let window = parseFloat(document.body.clientWidth);
+        if (window > 1024) {
+            mobileMenu.style.display = "none";
+            mobileMenu.classList.remove("menuOpen");
+        }
+}
+    );
 });
